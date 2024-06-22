defb3b1a22cdaab5ff0e1fa31b11c3e8b1f86e4e
ashutoshsharan2003 <141537249+ashutoshsharan2003@users.noreply.github.com>
2024-06-22 08:42:16 +0530

    Add files via upload

---
+++ pip installer.py
@@ -0,0 +1,10 @@
+pip install wolframalpha
+pip install wikipedia
+pip install ecapture
+pip install twilio
+pip install beautifulsoup4
+pip install winshell
+pip install pyjokes
+pip install feedparser
+pip install clint
+pip install SpeechRecognition pyttsx3
